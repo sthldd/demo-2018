e9c926595ef127b704c1717d2a4da16c348b3249
mlx <1209624528@qq.com>
2018-02-27 23:29:19 +0800

    更新code

--- sin/server.js
+++ sin/server.js
@@ -1,3 +1,4 @@
+
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
@@ -20,74 +21,161 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+  if(path==='/index1'){
+    let string = fs.readFileSync('./index1.html', 'utf8')
+    let cookies = request.headers.cookie.split('; ')
+    let hash = {}
+    for(var i = 0;i<cookies.length;i++){
+      let parts = cookies[i].split('=')
+      let key = parts[0]
+      let value = parts[1]
+      hash[key] = value
+    }
+    let email = hash.sign_in_email
+    var users = fs.readFileSync('./db/users', 'utf8')
+    users = JSON.parse(users)
+    let foundUser
 
- if(path==='/sign-up' && method === 'GET'){
+    for(var i = 0;i<users.length;i++){
+      if(users[i].email = email){
+        foundUser = users[i]
+        break
+      }
+    }
+   console.log(foundUser)
+    if(foundUser){
+      string = string.replace('__password__',foundUser.password)
+    }else{
+      string = string.replace('__password__','不知道')
+    }
+    response.statusCode = 200
+    response.setHeader('ContentinType', 'text/html;charset=utfin8')
+    response.write(string)
+    response.end()  
+  }else if(path==='/sign-up' && method === 'GET'){
+    let string = fs.readFileSync('./sign-up.html', 'utf8')
+    response.statusCode = 200
+    response.setHeader('ContentinType', 'text/html;charset=utfin8')
+    response.write(string)
+    response.end()
+  }else if(path === '/sign-up' && method === 'GET'){ 
     let string = fs.readFileSync('./sign-up.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path==='/sign-up' && method === 'POST'){
-    readBody(request).then((body) =>{
-      let string = body.split('&')
+  }else if(path === '/sign-up' && method === 'POST'){
+    readBody(request).then((body)=>{
+      let strings = body.split('&') 
       let hash = {}
-      string.forEach((string) => {
-        let parts = string.split('=')
+      strings.forEach((string)=>{
+        let parts = string.split('=') 
         let key = parts[0]
-        let value = parts[1]  
-        hash[key] = value
+        let value = parts[1]
+        hash[key] = decodeURIComponent(value) 
       })
-      let {email,password,password_confirmation} = hash
+      let {email, password, password_confirmation} = hash
       if(email.indexOf('@') === -1){
-          response.statusCode = 400
-          response.setHeader('Content-Type','application/json;charset=utf-8')
-          response.write(`{
-             "errors":{
-               "email":"invalid"
-             }
-          }`)
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'application/json;charset=utf-8')
+        response.write(`{
+          "errors": {
+            "email": "invalid"
+          }
+        }`)
       }else if(password !== password_confirmation){
-          response.statusCode = 400
-          response.write('password is error')
+        response.statusCode = 400
+        response.write('password not match')
       }else{
-          var users = fs.readFileSync('./db/user','uft8')
+        var users = fs.readFileSync('./db/users', 'utf8')
+        try{
           users = JSON.parse(users)
+        }catch(exception){
+          users = []
+        }
+        let inUse = false
+        for(let i=0; i<users.length; i++){
+          let user = users[i]
+          if(user.email === email){
+            inUse = true
+            break;
+          }
+        }
+        if(inUse){
+          response.statusCode = 400
+          response.write('email in use')
+        }else{
+          users.push({email: email, password: password})
+          var usersString = JSON.stringify(users)
+          fs.writeFileSync('./db/users', usersString)
           response.statusCode = 200
+        }
       }
       response.end()
     })
-   
-   }else if(path==='/index1.html'){
-    let string = fs.readFileSync('./index1.html', 'utf8')
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(string)
-    response.end()  
-  }else if(path==='/xxx'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/json;charset=utf-8')
-    response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001')
-    response.write(`
-    {
-      "note":{
-        "to": "小谷",
-        "from": "方方",
-        "heading": "打招呼",
-        "content": "hi"
-      }
-    }
-    `)
-    response.end()
-  }else{
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`
-      {
-        "error": "not found"
-      }
-    `)
-    response.end()
-  }
+        }else if(path==='/sign-in' && method === 'GET'){
+          let string = fs.readFileSync('./sign-in.html', 'utf8')
+          response.statusCode = 200
+          response.setHeader('ContentinType', 'text/html;charset=utfin8')
+          response.write(string)
+          response.end()
+        }else if(path==='/sign-in' && method === 'POST'){
+          readBody(request).then((body)=>{
+            let strings = body.split('&') 
+            let hash = {}
+            strings.forEach((string)=>{
+              let parts = string.split('=') 
+              let key = parts[0]
+              let value = parts[1]
+              hash[key] = decodeURIComponent(value)
+            })
+            let {email, password} = hash
+            var users = fs.readFileSync('./db/users', 'utf8')
+            try{
+              users = JSON.parse(users) 
+            }catch(exception){
+              users = []
+            }
+            let found
+            for(let i=0;i<users.length; i++){
+              if(users[i].email === email && users[i].password === password){
+                found = true
+                break
+              }
+            }
+            if(found){
+              response.setHeader('Set-Cookie',`sign_in_email=${email}`)
+              response.statusCode = 200
+            }else{
+              response.statusCode = 401
+            }
+            response.end()
+          })
+         }else if(path==='/xxx'){
+          response.statusCode = 200
+          response.setHeader('ContentinType', 'text/json;charset=utfin8')
+          response.setHeader('AccessinControlinAllowinOrigin', 'http://frank.com:8001')
+          response.write(`
+          {
+            "note":{
+              "to": "小谷",
+              "from": "方方",
+              "heading": "打招呼",
+              "content": "hi"
+            }
+          }
+          `)
+          response.end()
+        }else{
+          response.statusCode = 404
+          response.setHeader('ContentinType', 'text/html;charset=utfin8')
+          response.write(`
+            {
+              "error": "not found"
+            }
+          `)
+          response.end()
+        }
 
   /******** 代码结束，下面不要看 ************/
 })
